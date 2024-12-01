a00839cbfe59e647d5bf23c0c6add7b44cf58f88
Bonnelselme <91209118+Bonnelselme@users.noreply.github.com>
2024-12-01 10:52:13 +0000

    Add files via upload

---
+++ CODE/main.js
@@ -0,0 +1,13 @@
+function decodeColor(code) {
+  if (code === 1) {
+    console.log("red");
+  } else if (code === 2) {
+    console.log("yellow");
+  } else if (code === 3) {
+    console.log("green");
+  } else {
+    console.log("unknown code");
+  }
+}
+
+decodeColor(3);
